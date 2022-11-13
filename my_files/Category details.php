9d2864466b204183cc5a502d337e82f06c11a861
reymur <ni-ttg@hotmail.com>
2022-11-13 04:35:08 +0400

    Latest Version-3 on Lenovo noutbook

--- my_files/Category details.php
+++ my_files/Category details.php
@@ -4233,9 +4233,181 @@ $phone_model_option = [
     ['phone_model_id' => 90, 'memory' => '512GB/6GB'],/*Apple Iphone 14 Pro*/
     ['phone_model_id' => 90, 'memory' => '1TB/6GB'],/*Apple Iphone 14 Pro*/
     ['phone_model_id' => 90, 'sim' => 'Dual Sim'],/*Apple Iphone 14 Pro*/
+
+    ['phone_model_id' => 91, 'color' => 'Space Black'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'color' => 'Silver'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'color' => 'Gold'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'color' => 'Deep Purple'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'memory' => '128GB/6GB'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'memory' => '256GB/6GB'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'memory' => '512GB/6GB'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'memory' => '1TB/6GB'],/*Apple Iphone 14 Pro Max*/
+    ['phone_model_id' => 91, 'sim' => 'Dual Sim'],/*Apple Iphone 14 Pro Max*/
+
+    ['phone_model_id' => 92, 'color' => 'Black'],/*Apple 3GS*/
+    ['phone_model_id' => 92, 'color' => 'White'],/*Apple 3GS*/
+    ['phone_model_id' => 92, 'memory' => '8GB'],/*Apple 3GS*/
+    ['phone_model_id' => 92, 'memory' => '16GB'],/*Apple 3GS*/
+    ['phone_model_id' => 92, 'memory' => '32GB'],/*Apple 3GS*/
+
+    ['phone_model_id' => 93, 'color' => 'Black'],/*Apple 4*/
+    ['phone_model_id' => 93, 'color' => 'White'],/*Apple 4*/
+    ['phone_model_id' => 93, 'memory' => '8GB'],/*Apple 4*/
+    ['phone_model_id' => 93, 'memory' => '16GB'],/*Apple 4*/
+    ['phone_model_id' => 93, 'memory' => '32GB'],/*Apple 4*/
+
+    ['phone_model_id' => 94, 'color' => 'Black'],/*Apple 4S*/
+    ['phone_model_id' => 94, 'color' => 'White'],/*Apple 4S*/
+    ['phone_model_id' => 94, 'memory' => '8GB'],/*Apple 4S*/
+    ['phone_model_id' => 94, 'memory' => '16GB'],/*Apple 4S*/
+    ['phone_model_id' => 94, 'memory' => '32GB'],/*Apple 4S*/
+    ['phone_model_id' => 94, 'memory' => '64GB'],/*Apple 4S*/
+
+    ['phone_model_id' => 95, 'color' => 'Black/Slate'],/*Apple 5*/
+    ['phone_model_id' => 95, 'color' => 'White/Silver'],/*Apple 5*/
+    ['phone_model_id' => 95, 'memory' => '16GB'],/*Apple 5*/
+    ['phone_model_id' => 95, 'memory' => '32GB'],/*Apple 5*/
+    ['phone_model_id' => 95, 'memory' => '64GB'],/*Apple 5*/
+
+    ['phone_model_id' => 96, 'color' => 'Blue'],/*Apple 5C*/
+    ['phone_model_id' => 96, 'color' => 'Green'],/*Apple 5C*/
+    ['phone_model_id' => 96, 'color' => 'Pink'],/*Apple 5C*/
+    ['phone_model_id' => 96, 'color' => 'White'],/*Apple 5C*/
+    ['phone_model_id' => 96, 'color' => 'Yellow'],/*Apple 5C*/
+    ['phone_model_id' => 96, 'memory' => '8GB'],/*Apple 5C*/
+    ['phone_model_id' => 96, 'memory' => '16GB'],/*Apple 5C*/
+    ['phone_model_id' => 96, 'memory' => '32GB'],/*Apple 5C*/
+
+    ['phone_model_id' => 97, 'color' => 'Gold'],/*Apple 5S*/
+    ['phone_model_id' => 97, 'color' => 'Space Gray'],/*Apple 5S*/
+    ['phone_model_id' => 97, 'color' => 'White/Silver'],/*Apple 5S*/
+    ['phone_model_id' => 97, 'memory' => '16GB'],/*Apple 5S*/
+    ['phone_model_id' => 97, 'memory' => '32GB'],/*Apple 5S*/
+    ['phone_model_id' => 97, 'memory' => '64GB'],/*Apple 5S*/
+
+    ['phone_model_id' => 98, 'color' => 'Gold'],/*Apple 6*/
+    ['phone_model_id' => 98, 'color' => 'Silver'],/*Apple 6*/
+    ['phone_model_id' => 98, 'color' => 'Space Gray'],/*Apple 6*/
+    ['phone_model_id' => 98, 'memory' => '16GB'],/*Apple 6*/
+    ['phone_model_id' => 98, 'memory' => '32GB'],/*Apple 6*/
+    ['phone_model_id' => 98, 'memory' => '64GB'],/*Apple 6*/
+    ['phone_model_id' => 98, 'memory' => '128GB'],/*Apple 6*/
+
+    ['phone_model_id' => 99, 'color' => 'Gold'],/*Apple 6 Plus*/
+    ['phone_model_id' => 99, 'color' => 'Silver'],/*Apple 6 Plus*/
+    ['phone_model_id' => 99, 'color' => 'Space Gray'],/*Apple 6 Plus*/
+    ['phone_model_id' => 99, 'memory' => '16GB'],/*Apple 6 Plus*/
+    ['phone_model_id' => 99, 'memory' => '64GB'],/*Apple 6 Plus*/
+    ['phone_model_id' => 99, 'memory' => '128GB'],/*Apple 6 Plus*/
+
+    ['phone_model_id' => 100, 'color' => 'Gold'],/*Apple 6S*/
+    ['phone_model_id' => 100, 'color' => 'Rose Gold'],/*Apple 6S*/
+    ['phone_model_id' => 100, 'color' => 'Silver'],/*Apple 6S*/
+    ['phone_model_id' => 100, 'color' => 'Space Gray'],/*Apple 6S*/
+    ['phone_model_id' => 100, 'memory' => '16GB'],/*Apple 6S*/
+    ['phone_model_id' => 100, 'memory' => '32GB'],/*Apple 6S*/
+    ['phone_model_id' => 100, 'memory' => '64GB'],/*Apple 6S*/
+    ['phone_model_id' => 100, 'memory' => '128GB'],/*Apple 6S*/
+
+    ['phone_model_id' => 101, 'color' => 'Gold'],/*Apple 6S Plus*/
+    ['phone_model_id' => 101, 'color' => 'Rose Gold'],/*Apple 6S Plus*/
+    ['phone_model_id' => 101, 'color' => 'Silver'],/*Apple 6S Plus*/
+    ['phone_model_id' => 101, 'color' => 'Space Gray'],/*Apple 6S Plus*/
+    ['phone_model_id' => 101, 'memory' => '16GB'],/*Apple 6S Plus*/
+    ['phone_model_id' => 101, 'memory' => '32GB'],/*Apple 6S Plus*/
+    ['phone_model_id' => 101, 'memory' => '64GB'],/*Apple 6S Plus*/
+    ['phone_model_id' => 101, 'memory' => '128GB'],/*Apple 6S Plus*/
+
+    ['phone_model_id' => 102, 'color' => 'Black'],/*Apple 7*/
+    ['phone_model_id' => 102, 'color' => 'Gold'],/*Apple 7*/
+    ['phone_model_id' => 102, 'color' => 'Jet Black'],/*Apple 7*/
+    ['phone_model_id' => 102, 'color' => 'Red'],/*Apple 7*/
+    ['phone_model_id' => 102, 'color' => 'Rose Gold'],/*Apple 7*/
+    ['phone_model_id' => 102, 'color' => 'Silver'],/*Apple 7*/
+    ['phone_model_id' => 102, 'memory' => '32GB'],/*Apple 7*/
+    ['phone_model_id' => 102, 'memory' => '128GB'],/*Apple 7*/
+    ['phone_model_id' => 102, 'memory' => '256GB'],/*Apple 7*/
+
+    ['phone_model_id' => 103, 'color' => 'Black'],/*Apple 7 Plus*/
+    ['phone_model_id' => 102, 'color' => 'Gold'],/*Apple 7 Plus*/
+    ['phone_model_id' => 103, 'color' => 'Jet Black'],/*Apple 7 Plus*/
+    ['phone_model_id' => 103, 'color' => 'Red'],/*Apple 7 Plus*/
+    ['phone_model_id' => 103, 'color' => 'Rose Gold'],/*Apple 7 Plus*/
+    ['phone_model_id' => 103, 'color' => 'Silver'],/*Apple 7 Plus*/
+    ['phone_model_id' => 103, 'memory' => '32GB'],/*Apple 7 Plus*/
+    ['phone_model_id' => 103, 'memory' => '128GB'],/*Apple 7 Plus*/
+    ['phone_model_id' => 103, 'memory' => '256GB'],/*Apple 7 Plus*/
+
+    ['phone_model_id' => 104, 'color' => 'Gold'],/*Apple 8*/
+    ['phone_model_id' => 104, 'color' => 'Red'],/*Apple 8*/
+    ['phone_model_id' => 104, 'color' => 'Silver'],/*Apple 8*/
+    ['phone_model_id' => 104, 'color' => 'Space Gray'],/*Apple 8*/
+    ['phone_model_id' => 104, 'memory' => '64GB'],/*Apple 8*/
+    ['phone_model_id' => 104, 'memory' => '256GB'],/*Apple 8*/
+
+    ['phone_model_id' => 105, 'color' => 'Gold'],/*Apple 8 Plus*/
+    ['phone_model_id' => 105, 'color' => 'Red'],/*Apple 8 Plus*/
+    ['phone_model_id' => 105, 'color' => 'Silver'],/*Apple 8 Plus*/
+    ['phone_model_id' => 105, 'color' => 'Space Gray'],/*Apple 8 Plus*/
+    ['phone_model_id' => 105, 'memory' => '64GB'],/*Apple 8 Plus*/
+    ['phone_model_id' => 105, 'memory' => '256GB'],/*Apple 8 Plus*/
+
+    ['phone_model_id' => 106, 'color' => 'Gold'],/*Apple SE*/
+    ['phone_model_id' => 106, 'color' => 'Rose Gold'],/*Apple SE*/
+    ['phone_model_id' => 106, 'color' => 'Silver'],/*Apple SE*/
+    ['phone_model_id' => 106, 'color' => 'Space Gray'],/*Apple SE*/
+    ['phone_model_id' => 106, 'memory' => '16GB'],/*Apple SE*/
+    ['phone_model_id' => 106, 'memory' => '32GB'],/*Apple SE*/
+    ['phone_model_id' => 106, 'memory' => '64GB'],/*Apple SE*/
+    ['phone_model_id' => 106, 'memory' => '128GB'],/*Apple SE*/
+
+    ['phone_model_id' => 106, 'color' => 'Black'],/*Apple SE (2020)*/
+    ['phone_model_id' => 106, 'color' => 'Red'],/*Apple SE (2020)*/
+    ['phone_model_id' => 106, 'color' => 'White'],/*Apple SE (2020)*/
+    ['phone_model_id' => 106, 'memory' => '64GB/3GB'],/*Apple SE (2020)*/
+    ['phone_model_id' => 106, 'memory' => '128GB/3GB'],/*Apple SE (2020)*/
+    ['phone_model_id' => 106, 'memory' => '256GB/3GB'],/*Apple SE (2020)*/
+
+    ['phone_model_id' => 107, 'color' => 'Red'],/*Apple SE (2022)*/
+    ['phone_model_id' => 107, 'color' => 'Black'],/*Apple SE (2022)*/
+    ['phone_model_id' => 107, 'color' => 'White'],/*Apple SE (2022)*/
+    ['phone_model_id' => 107, 'memory' => '64GB/4GB'],/*Apple SE (2022)*/
+    ['phone_model_id' => 107, 'memory' => '128GB/4GB'],/*Apple SE (2022)*/
+    ['phone_model_id' => 107, 'memory' => '256GB/4GB'],/*Apple SE (2022)*/
+
+    ['phone_model_id' => 108, 'color' => 'Silver'],/*Apple X*/
+    ['phone_model_id' => 108, 'color' => 'Space Gray'],/*Apple X*/
+    ['phone_model_id' => 108, 'memory' => '64GB/3GB'],/*Apple X*/
+    ['phone_model_id' => 108, 'memory' => '128GB/3GB'],/*Apple X*/
+    ['phone_model_id' => 108, 'memory' => '256GB/3GB'],/*Apple X*/
+
+    ['phone_model_id' => 109, 'color' => 'Black'],/*Apple XR*/
+    ['phone_model_id' => 109, 'color' => 'Blue'],/*Apple XR*/
+    ['phone_model_id' => 109, 'color' => 'Coral'],/*Apple XR*/
+    ['phone_model_id' => 109, 'color' => 'Red'],/*Apple XR*/
+    ['phone_model_id' => 109, 'color' => 'White'],/*Apple XR*/
+    ['phone_model_id' => 109, 'color' => 'Yellow'],/*Apple XR*/
+    ['phone_model_id' => 109, 'memory' => '64GB/3GB'],/*Apple XR*/
+    ['phone_model_id' => 109, 'memory' => '128GB/3GB'],/*Apple XR*/
+    ['phone_model_id' => 109, 'memory' => '256GB/3GB'],/*Apple XR*/
+    ['phone_model_id' => 109, 'sim' => 'Dual Sim'],/*Apple XR*/
+
+    ['phone_model_id' => 110, 'color' => 'Gold'],/*Apple XS*/
+    ['phone_model_id' => 110, 'color' => 'Silver'],/*Apple XS*/
+    ['phone_model_id' => 110, 'color' => 'Space Gray'],/*Apple XS*/
+    ['phone_model_id' => 110, 'memory' => '64GB/4GB'],/*Apple XS*/
+    ['phone_model_id' => 110, 'memory' => '256GB/4GB'],/*Apple XS*/
+    ['phone_model_id' => 110, 'memory' => '512GB/4GB'],/*Apple XS*/
+
+    ['phone_model_id' => 111, 'color' => 'Gold'],/*Apple XS Max*/
+    ['phone_model_id' => 111, 'color' => 'Silver'],/*Apple XS Max*/
+    ['phone_model_id' => 111, 'color' => 'Space Gray'],/*Apple XS Max*/
+    ['phone_model_id' => 111, 'memory' => '64GB/4GB'],/*Apple XS Max*/
+    ['phone_model_id' => 111, 'memory' => '256GB/4GB'],/*Apple XS Max*/
+    ['phone_model_id' => 111, 'memory' => '512GB/4GB'],/*Apple XS Max*/
+    ['phone_model_id' => 111, 'sim' => 'Dual Sim'],/*Apple XS Max*/
 ];
 
-19"></option></select> 
 
 
 
