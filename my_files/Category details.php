01d3693bb5eaddffe46fb0ca922226a4dea86e85
reymur <ni-ttg@hotmail.com>
2022-11-14 14:16:33 +0400

    Latest Version-4 on Lenovo noutbook

--- my_files/Category details.php
+++ my_files/Category details.php
@@ -816,7 +816,7 @@ $phone_model = [
     ['sub_category_id' => 205,'name' => "Volt"],/*Blackberry*/
     ['sub_category_id' => 205,'name' => "Z10"],/*Blackberry*/
     ['sub_category_id' => 205,'name' => "Z3"],/*Blackberry*/
-    ['sub_category_id' => 205,'name' => "Z30"],/*Blackberry*/
+    ['sub_category_id' => 205,'name' => "Z30"],/*id 152 Blackberry*/
     ['sub_category_id' => 206,'name' => "BL12000"],/*Doogee*/
     ['sub_category_id' => 206,'name' => "BL12000 Pro"],/*Doogee*/
     ['sub_category_id' => 206,'name' => "BL5000"],/*Doogee*/
@@ -875,8 +875,8 @@ $phone_model = [
     ['sub_category_id' => 206,'name' => "Y8"],/*Doogee*/
     ['sub_category_id' => 206,'name' => "Y8 Plus"],/*Doogee*/
     ['sub_category_id' => 206,'name' => "Y8C"],/*Doogee*/
-    ['sub_category_id' => 206,'name' => "Y9 Plus"],/*Doogee*/
-    ['sub_category_id' => 208,'name' => "GF 788"],/*Ericsson*/
+    ['sub_category_id' => 206,'name' => "Y9 Plus"],/*id 211 Doogee*/
+    ['sub_category_id' => 208,'name' => "GF 788"],/*id 212 Ericsson*/
     ['sub_category_id' => 208,'name' => "GF 788e"],/*Ericsson*/
     ['sub_category_id' => 208,'name' => "R310s"],/*Ericsson*/
     ['sub_category_id' => 208,'name' => "R320"],/*Ericsson*/
@@ -889,7 +889,7 @@ $phone_model = [
     ['sub_category_id' => 208,'name' => "T39"],/*Ericsson*/
     ['sub_category_id' => 208,'name' => "T65"],/*Ericsson*/
     ['sub_category_id' => 208,'name' => "T66"],/*Ericsson*/
-    ['sub_category_id' => 208,'name' => "T68"],/*Ericsson*/
+    ['sub_category_id' => 208,'name' => "T68"],/*id 225 Ericsson*/
     ['sub_category_id' => 209,'name' => "B240"],/*Fly*/
     ['sub_category_id' => 209,'name' => "Champ"],/*Fly*/
     ['sub_category_id' => 209,'name' => "Cirrus 1"],/*Fly*/
@@ -967,8 +967,8 @@ $phone_model = [
     ['sub_category_id' => 209,'name' => "Tornado Slim"],/*Fly*/
     ['sub_category_id' => 209,'name' => "Tornado TS113"],/*Fly*/
     ['sub_category_id' => 209,'name' => "View"],/*Fly*/
-    ['sub_category_id' => 209,'name' => "View Max"],/*Fly*/
-    ['sub_category_id' => 210,'name' => "C20"],/*Hisense*/
+    ['sub_category_id' => 209,'name' => "View Max"],/*id 303 Fly*/
+    ['sub_category_id' => 210,'name' => "C20"],/*id 304 Hisense*/
     ['sub_category_id' => 210,'name' => "C30"],/*Hisense*/
     ['sub_category_id' => 210,'name' => "F10"],/*Hisense*/
     ['sub_category_id' => 210,'name' => "F17 Pro"],/*Hisense*/
@@ -1064,7 +1064,7 @@ $phone_model = [
     ['sub_category_id' => 213,'name' => '8A Prime'],/*Honor*/
     ['sub_category_id' => 213,'name' => '8A Pro'],/*Honor*/
     ['sub_category_id' => 213,'name' => '8C'],/*Honor*/
-    ['sub_category_id' => 213,'name' => '8S'],/*Honor*/
+    ['sub_category_id' => 213,'name' => '8S'],/*id 400 Honor*/
     ['sub_category_id' => 213,'name' => '8S 2020'],/*Honor*/
     ['sub_category_id' => 213,'name' => '8X'],/*Honor*/
     ['sub_category_id' => 213,'name' => '8X 5G'],/*Honor*/
@@ -1114,7 +1114,7 @@ $phone_model = [
     ['sub_category_id' => 213,'name' => 'Play4 Pro'],/*Honor*/
     ['sub_category_id' => 213,'name' => 'Tab 5'],/*Honor*/
     ['sub_category_id' => 213,'name' => 'U8860'],/*Honor*/
-    ['sub_category_id' => 213,'name' => 'V30'],/*Honor*/
+    ['sub_category_id' => 213,'name' => 'V30'],/*id 450 Honor*/
     ['sub_category_id' => 213,'name' => 'V30 Pro'],/*Honor*/
     ['sub_category_id' => 213,'name' => 'V40 5G'],/*Honor*/
     ['sub_category_id' => 213,'name' => 'V40 Lite'],/*Honor*/
@@ -1136,7 +1136,7 @@ $phone_model = [
     ['sub_category_id' => 213,'name' => 'X8'],/*Honor*/
     ['sub_category_id' => 213,'name' => 'X8 5G'],/*Honor*/
     ['sub_category_id' => 213,'name' => 'X9'],/*Honor*/
-    ['sub_category_id' => 213,'name' => 'X9 5G'],/*Honor*/
+    ['sub_category_id' => 213,'name' => 'X9 5G'],/*id 472 Honor*/
     ['sub_category_id' => 214,'name' => '2019'],/*Hope*/
     ['sub_category_id' => 214,'name' => '2020'],/*Hope*/
     ['sub_category_id' => 214,'name' => 'A10'],/*Hope*/
@@ -1164,7 +1164,7 @@ $phone_model = [
     ['sub_category_id' => 214,'name' => 'S35'],/*Hope*/
     ['sub_category_id' => 214,'name' => 'S66 Mini'],/*Hope*/
     ['sub_category_id' => 214,'name' => 'S68 Mini'],/*Hope*/
-    ['sub_category_id' => 214,'name' => 'S70'],/*Hope*/
+    ['sub_category_id' => 214,'name' => 'S70'],/*id 500 Hope*/
     ['sub_category_id' => 214,'name' => 'S77'],/*Hope*/
     ['sub_category_id' => 214,'name' => 'S800'],/*Hope*/
     ['sub_category_id' => 214,'name' => 'SQ'],/*Hope*/
@@ -1173,8 +1173,8 @@ $phone_model = [
     ['sub_category_id' => 214,'name' => 'T55'],/*Hope*/
     ['sub_category_id' => 214,'name' => 'V1'],/*Hope*/
     ['sub_category_id' => 214,'name' => 'V3'],/*Hope*/
-    ['sub_category_id' => 214,'name' => 'V8'],/*Hope*/
-    ['sub_category_id' => 215,'name' => '10'],/*HTC*/
+    ['sub_category_id' => 214,'name' => 'V8'],/*id 509 Hope*/
+    ['sub_category_id' => 215,'name' => '10'],/*id 510 HTC*/
     ['sub_category_id' => 215,'name' => '10 evo'],/*HTC*/
     ['sub_category_id' => 215,'name' => '7 Pro'],/*HTC*/
     ['sub_category_id' => 215,'name' => '7 Surround'],/*HTC*/
@@ -4406,7 +4406,205 @@ $phone_model_option = [
     ['phone_model_id' => 111, 'memory' => '256GB/4GB'],/*Apple XS Max*/
     ['phone_model_id' => 111, 'memory' => '512GB/4GB'],/*Apple XS Max*/
     ['phone_model_id' => 111, 'sim' => 'Dual Sim'],/*Apple XS Max*/
+
+    ['phone_model_id' => 112, 'color' => 'Black'],/*Blackberry 4G LTY Playbook*/
+    ['phone_model_id' => 112, 'memory' => '32GB/1GB'],/*Blackberry 4G LTY Playbook*/
+
+    ['phone_model_id' => 113, 'color' => 'Black'],/*Blackberry 9720*/
+    ['phone_model_id' => 113, 'color' => 'White'],/*Blackberry 9720*/
+    ['phone_model_id' => 113, 'color' => 'Purple'],/*Blackberry 9720*/
+    ['phone_model_id' => 113, 'color' => 'Blue'],/*Blackberry 9720*/
+    ['phone_model_id' => 113, 'color' => 'Pure Pink'],/*Blackberry 9720*/
+
+    ['phone_model_id' => 114, 'color' => 'Black'],/*Blackberry Aurora*/
+    ['phone_model_id' => 114, 'color' => 'Silver'],/*Blackberry Aurora*/
+    ['phone_model_id' => 114, 'color' => 'Gold'],/*Blackberry Aurora*/
+    ['phone_model_id' => 114, 'memory' => '32GB/4GB'],/*Blackberry Aurora*/
+
+    ['phone_model_id' => 115, 'color' => 'Black'],/*Blackberry Bold 9790*/
+    ['phone_model_id' => 115, 'color' => 'White'],/*Blackberry Bold 9790*/
+    ['phone_model_id' => 115, 'memory' => '8GB'],/*Blackberry Bold 9790*/
+
+    ['phone_model_id' => 116, 'color' => 'Black'],/*Blackberry Bold Touch 9900*/
+    ['phone_model_id' => 116, 'color' => 'White'],/*Blackberry Bold Touch 9900*/
+    ['phone_model_id' => 116, 'memory' => '8GB'],/*Blackberry Bold Touch 9900*/
+
+    ['phone_model_id' => 117, 'color' => 'Black'],/*Blackberry Bold Touch 9930*/
+    ['phone_model_id' => 117, 'memory' => '8GB'],/*Blackberry Bold Touch 9930*/
+
+    ['phone_model_id' => 118, 'color' => 'Black'],/*Blackberry Classic*/
+    ['phone_model_id' => 118, 'color' => 'White'],/*Blackberry Classic*/
+    ['phone_model_id' => 118, 'color' => 'Blue'],/*Blackberry Classic*/
+    ['phone_model_id' => 118, 'color' => 'Bronze'],/*Blackberry Classic*/
+    ['phone_model_id' => 118, 'color' => 'Cobalt Blue'],/*Blackberry Classic*/
+    ['phone_model_id' => 118, 'memory' => '16GB/2GB'],/*Blackberry Classic*/
+
+    ['phone_model_id' => 119, 'color' => 'Black'],/*Blackberry Classic Non camera*/
+    ['phone_model_id' => 119, 'memory' => '16GB/2GB'],/*Blackberry Classic Non camera*/
+
+    ['phone_model_id' => 120, 'color' => 'Black'],/*Blackberry Curve 8980*/
+
+    ['phone_model_id' => 121, 'color' => 'Black'],/*Blackberry Curve 9220*/
+    ['phone_model_id' => 121, 'color' => 'Fuchsia Pink'],/*Blackberry Curve 9220*/
+    ['phone_model_id' => 121, 'color' => 'Pure White'],/*Blackberry Curve 9220*/
+    ['phone_model_id' => 121, 'color' => 'Teal Blue'],/*Blackberry Curve 9220*/
+
+    ['phone_model_id' => 122, 'color' => 'Black'],/*Blackberry Curve 9320*/
+    ['phone_model_id' => 122, 'color' => 'White'],/*Blackberry Curve 9320*/
+
+    ['phone_model_id' => 123, 'color' => 'Black'],/*Blackberry Curve 9350*/
+    ['phone_model_id' => 123, 'color' => 'Violet'],/*Blackberry Curve 9350*/
+
+    ['phone_model_id' => 124, 'color' => 'Black'],/*Blackberry Curve 9360*/
+    ['phone_model_id' => 124, 'color' => 'Purple'],/*Blackberry Curve 9360*/
+    ['phone_model_id' => 124, 'color' => 'Pink'],/*Blackberry Curve 9360*/
+
+    ['phone_model_id' => 125, 'color' => 'Black'],/*Blackberry Curve 9370*/
+
+    ['phone_model_id' => 126, 'color' => 'Black'],/*Blackberry Curve 9380*/
+
+    ['phone_model_id' => 127, 'color' => 'Black'],/*Blackberry Curve Touch*/
+
+    ['phone_model_id' => 128, 'color' => 'Black'],/*Blackberry DTEK50*/
+    ['phone_model_id' => 128, 'memory' => '16GB/3GB'],/*Blackberry DTEK50*/
+
+    ['phone_model_id' => 129, 'color' => 'Black'],/*Blackberry DTEK60*/
+    ['phone_model_id' => 129, 'memory' => '32GB/4GB'],/*Blackberry DTEK60*/
+
+    ['phone_model_id' => 130, 'color' => 'Black'],/*Blackberry Evolve*/
+    ['phone_model_id' => 130, 'memory' => '64GB/4GB'],/*Blackberry Evolve*/
+
+    ['phone_model_id' => 131, 'color' => 'Black'],/*Blackberry Evolve X*/
+    ['phone_model_id' => 131, 'memory' => '64GB/6GB'],/*Blackberry Evolve X*/
+
+    ['phone_model_id' => 132, 'color' => 'Black'],/*Blackberry KEY2*/
+    ['phone_model_id' => 132, 'color' => 'Silver'],/*Blackberry KEY2*/
+    ['phone_model_id' => 132, 'color' => 'Red'],/*Blackberry KEY2*/
+    ['phone_model_id' => 132, 'memory' => '64GB/6GB'],/*Blackberry KEY2*/
+    ['phone_model_id' => 132, 'memory' => '128GB/6GB'],/*Blackberry KEY2*/
+
+    ['phone_model_id' => 133, 'color' => 'Slate'],/*Blackberry KEY2 LE*/
+    ['phone_model_id' => 133, 'color' => 'Champagne'],/*Blackberry KEY2 LE*/
+    ['phone_model_id' => 133, 'color' => 'Atomic'],/*Blackberry KEY2 LE*/
+    ['phone_model_id' => 133, 'memory' => '32GB/4GB'],/*Blackberry KEY2 LE*/
+    ['phone_model_id' => 133, 'memory' => '64GB/4GB'],/*Blackberry KEY2 LE*/
+
+    ['phone_model_id' => 134, 'color' => 'Black/Silver'],/*Blackberry Keyone*/
+    ['phone_model_id' => 134, 'color' => 'Black'],/*Blackberry Keyone*/
+    ['phone_model_id' => 134, 'color' => 'Bronze'],/*Blackberry Keyone*/
+    ['phone_model_id' => 134, 'memory' => '32GB/4GB'],/*Blackberry Keyone*/
+    ['phone_model_id' => 134, 'memory' => '64GB/4GB'],/*Blackberry Keyone*/
+
+    ['phone_model_id' => 135, 'color' => 'Black'],/*Blackberry leap*/
+    ['phone_model_id' => 135, 'color' => 'White'],/*Blackberry leap*/
+    ['phone_model_id' => 135, 'memory' => '16GB/2GB'],/*Blackberry leap*/
+
+    ['phone_model_id' => 136, 'color' => 'Black'],/*Blackberry Motion*/
+    ['phone_model_id' => 136, 'memory' => '32GB/4GB'],/*Blackberry Motion*/
+
+    ['phone_model_id' => 137, 'color' => 'Black'],/*Blackberry Passport*/
+    ['phone_model_id' => 137, 'color' => 'White'],/*Blackberry Passport*/
+    ['phone_model_id' => 137, 'color' => 'Red'],/*Blackberry Passport*/
+    ['phone_model_id' => 137, 'color' => 'Black Gold'],/*Blackberry Passport*/
+    ['phone_model_id' => 137, 'color' => 'Silver'],/*Blackberry Passport*/
+    ['phone_model_id' => 137, 'memory' => '32GB/3GB'],/*Blackberry Passport*/
+
+    ['phone_model_id' => 138, 'color' => 'Black'],/*Blackberry Porsche Design D'9531*/
+    ['phone_model_id' => 138, 'memory' => '8GB'],/*Blackberry Porsche Design D'9531*/
+
+    ['phone_model_id' => 139, 'color' => 'Gray'],/*Blackberry Porsche Design P'9981*/
+    ['phone_model_id' => 139, 'color' => 'Black'],/*Blackberry Porsche Design P'9981*/
+    ['phone_model_id' => 139, 'memory' => '8GB'],/*Blackberry Porsche Design P'9981*/
+
+    ['phone_model_id' => 140, 'color' => 'Silver'],/*Blackberry Porsche Design P'9982*/
+    ['phone_model_id' => 140, 'memory' => '64GB/2GB'],/*Blackberry Porsche Design P'9982*/
+
+    ['phone_model_id' => 141, 'color' => 'Black'],/*Blackberry Porsche Design P'9983*/
+    ['phone_model_id' => 141, 'memory' => '64GB/2GB'],/*Blackberry Porsche Design P'9983*/
+
+    ['phone_model_id' => 142, 'color' => 'Black'],/*Blackberry Priv*/
+    ['phone_model_id' => 142, 'memory' => '32GB/3GB'],/*Blackberry Priv*/
+
+    ['phone_model_id' => 143, 'color' => 'Black'],/*Blackberry Q10*/
+    ['phone_model_id' => 143, 'color' => 'White'],/*Blackberry Q10*/
+    ['phone_model_id' => 143, 'memory' => '16GB/2GB'],/*Blackberry Q10*/
+
+    ['phone_model_id' => 144, 'color' => 'Black'],/*Blackberry Q5*/
+    ['phone_model_id' => 144, 'color' => 'White'],/*Blackberry Q5*/
+    ['phone_model_id' => 144, 'color' => 'Red'],/*Blackberry Q5*/
+    ['phone_model_id' => 144, 'color' => 'Gray'],/*Blackberry Q5*/
+    ['phone_model_id' => 144, 'color' => 'Pink'],/*Blackberry Q5*/
+    ['phone_model_id' => 144, 'memory' => '8GB/2GB'],/*Blackberry Q5*/
+
+    ['phone_model_id' => 145, 'color' => 'Black'],/*Blackberry Storm3*/
+    ['phone_model_id' => 145, 'memory' => '8GB'],/*Blackberry Storm3*/
+
+    ['phone_model_id' => 146, 'color' => 'Black'],/*Blackberry Touch 9810*/
+    ['phone_model_id' => 146, 'color' => 'Gray'],/*Blackberry Touch 9810*/
+    ['phone_model_id' => 146, 'memory' => '8GB'],/*Blackberry Touch 9810*/
+
+    ['phone_model_id' => 147, 'color' => 'Black'],/*Blackberry Touch 9850*/
+    ['phone_model_id' => 147, 'memory' => '4GB'],/*Blackberry Touch 9850*/
+
+    ['phone_model_id' => 148, 'color' => 'Black'],/*Blackberry Touch 9860*/
+    ['phone_model_id' => 148, 'memory' => '4GB'],/*Blackberry Touch 9860*/
+
+    ['phone_model_id' => 149, 'color' => 'Black'],/*Blackberry Volt*/
+    ['phone_model_id' => 149, 'memory' => '4GB'],/*Blackberry Volt*/
+
+    ['phone_model_id' => 150, 'color' => 'Black'],/*Blackberry Z10*/
+    ['phone_model_id' => 150, 'color' => 'White'],/*Blackberry Z10*/
+    ['phone_model_id' => 150, 'memory' => '16GB/2GB'],/*Blackberry Z10*/
+
+    ['phone_model_id' => 151, 'color' => 'Black'],/*Blackberry Z3*/
+    ['phone_model_id' => 151, 'memory' => '8GB'],/*Blackberry Z3*/
+
+    ['phone_model_id' => 152, 'color' => 'Black'],/*Blackberry Z30*/
+    ['phone_model_id' => 152, 'memory' => '16GB/2GB'],/*Blackberry Z30*/
+
+    ['phone_model_id' => 214, 'color' => 'Bright Orange'],/*Ericsson R310s*/
+    ['phone_model_id' => 214, 'color' => 'Nautic Blue'],/*Ericsson R310s*/
+    ['phone_model_id' => 214, 'color' => 'Aquatic Green and Peak Yellow'],/*Ericsson R310s*/
+
+    ['phone_model_id' => 215, 'color' => 'Atlantic Blue'],/*Ericsson R320*/
+    ['phone_model_id' => 215, 'color' => 'Rock Red and Stone Purple'],/*Ericsson R320*/
+
+    ['phone_model_id' => 217, 'color' => 'Ice Blue'],/*Ericsson R600*/
+    ['phone_model_id' => 217, 'color' => 'Luminous Champagne'],/*Ericsson R600*/
+
+    ['phone_model_id' => 221, 'color' => 'Amethyst Purple and Sapphire Blue'],/*Ericsson T29s*/
+
+    ['phone_model_id' => 222, 'color' => 'Classic Blue'],/*Ericsson T39*/
+    ['phone_model_id' => 222, 'color' => 'Icecap Blue'],/*Ericsson T39*/
+    ['phone_model_id' => 222, 'color' => 'Rose White'],/*Ericsson T39*/
+
+    ['phone_model_id' => 223, 'color' => 'Cosmic Blue'],/*Ericsson T65*/
+    ['phone_model_id' => 223, 'color' => 'Stardust Yellow'],/*Ericsson T65*/
+    ['phone_model_id' => 223, 'color' => 'Polar Blue'],/*Ericsson T65*/
+
+    ['phone_model_id' => 224, 'color' => 'Purple Passion'],/*Ericsson T66*/
+    ['phone_model_id' => 224, 'color' => 'Silver Supreme'],/*Ericsson T66*/
+
+    ['phone_model_id' => 225, 'color' => 'Zirocñan Gold'],/*Ericsson T68*/
+    ['phone_model_id' => 225, 'color' => 'Lunar Gray'],/*Ericsson T68*/
+
+    ['phone_model_id' => 510, 'color' => 'Carbon Gray'],/*HTC 10*/
+    ['phone_model_id' => 510, 'color' => 'Topaz Gold'],/*HTC 10*/
+    ['phone_model_id' => 510, 'color' => 'Glacier Silver'],/*HTC 10*/
+    ['phone_model_id' => 510, 'color' => 'Camellia Red'],/*HTC 10*/
+    ['phone_model_id' => 510, 'color' => 'Camellia Red'],/*HTC 10*/
+    ['phone_model_id' => 510, 'memory' => '32GB/4GB'],/*HTC 10*/
+    ['phone_model_id' => 510, 'memory' => '64GB/4GB'],/*HTC 10*/
+
+    ['phone_model_id' => 511, 'color' => 'Silver'],/*HTC 10 evo*/
+    ['phone_model_id' => 511, 'color' => 'Black'],/*HTC 10 evo*/
+    ['phone_model_id' => 511, 'memory' => '32GB/3GB'],/*HTC 10 evo*/
+    ['phone_model_id' => 511, 'memory' => '64GB/3GB'],/*HTC 10 evo*/
+
+    ['phone_model_id' => 512, 'color' => 'Black'],/*HTC 7 Pro*/
+    ['phone_model_id' => 512, 'memory' => '8GB'],/*HTC 7 Pro*/
 ];
+
 
 
 
