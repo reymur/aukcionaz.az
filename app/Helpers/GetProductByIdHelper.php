4c794d9bfa14304c8ead744e6da8df3cd31f358a
Reymur <reymur@Reymurs-MacBook-Pro.local>
2023-03-24 04:08:12 +0400

    in mac os monterey latest version 4

---
+++ app/Helpers/GetProductByIdHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Product;
+
+    function GetProductById($id) {
+        if( !empty($id) ) {
+            $product = Product::where('id',$id)->first();
+
+            if( ! $product ) abort(404 );
+
+            if( $product && $product->id && $product->productable ) {
+                $product_infos = $product->productable->with(['user','images','phones'])
+                    ->where('id',$product->productable->id)->first();
+
+                if( $product_infos && $product_infos->id ) return $product_infos;
+                else return false;
+            }
+            return false;
+        }
+        return false;
+    }
---
+++ resources/views/announce/witing_announce.blade.php
@@ -0,0 +1,12 @@
+
+<div class="">
+    <div class="">
+        <!-- NEW ANNOUNCE INFO -->
+        <div class="">
+
+        </div>
+    </div>
+
+    <!-- ADD AUKCION -->
+    <div class=""></div>
+</div>
