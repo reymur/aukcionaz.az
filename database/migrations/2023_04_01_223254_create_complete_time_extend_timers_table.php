5b32cdbf85972b4ed26d8dfd4edeeed34c7cd9c3
Reymur <reymur@Reymurs-MacBook-Pro.local>
2023-04-02 04:08:43 +0400

    in mac os monterey latest version 26

---
+++ database/migrations/2023_04_01_223254_create_complete_time_extend_timers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompleteTimeExtendTimersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complete_time_extend_timers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complete_time_extend_timers');
+    }
+}
